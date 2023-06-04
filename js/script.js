99f641ce7e05a3e9ad5f6c3b7eaba63a8ac93452
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-04 15:53:02 +0200

    Define character sets for generating password

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+// Add click event listener to generate button
+generateButton.addEventListener("click", generatePassword);
+
 // Get references to the password range input and display input
-let passwordRange = document.getElementById('password-range');
-let displayPasswordLength = document.getElementById('display-password-length');
+const passwordRange = document.getElementById('password-range');
+const displayPasswordLength = document.getElementById('display-password-length');
 
 // Add event listener for the input event on the password range input
-passwordRange.addEventListener('input', function() {
+passwordRange.addEventListener('input', function () {
     // Update the value of the display input with the range input value
     displayPasswordLength.value = this.value;
 });
+
+//-----------------------------------
+
+// Define character sets for generating password
+const dataLowercase = "azertyuiopmlkjhgfdsqwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "$*ù%&é\"'(-è_çà)=^¨£¤*?:;,./§!<>|";
+const rangeValue = document.getElementById("password-range");
+
+
+
